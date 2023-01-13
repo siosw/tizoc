dae634ca4c672b7fa60175c6491a12304e6e4dd8
siosw <git@simonoswald.xyz>
2023-01-13 15:05:50 +0100

    feat: simple transaction table

--- src/Transactions.tsx
+++ src/Transactions.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import {
     AztecSdkUser,
     UserDefiTx,
+    UserDefiClaimTx,
     UserPaymentTx,
     UserAccountTx,
 } from '@aztec/sdk'
@@ -13,11 +14,44 @@ type TransactionsProps = {
 
 type ReadableTx = {
     txId: String
+    txType: String
+    txValue: String
     // value: Number
 }
 
 function Transactions({ user }: TransactionsProps) {
     const [txs, setTxs] = useState<Array<ReadableTx>>()
+
+    function getTxType(
+        tx: UserDefiTx | UserDefiClaimTx | UserAccountTx | UserPaymentTx
+    ): String {
+        switch (true) {
+            case tx instanceof UserDefiTx: {
+                return 'defi'
+            }
+            case tx instanceof UserDefiClaimTx: {
+                return 'defi claim'
+            }
+            case tx instanceof UserPaymentTx: {
+                return 'payment'
+            }
+            case tx instanceof UserAccountTx: {
+                return 'account'
+            }
+        }
+
+        return 'unknown type'
+    }
+
+    function getTxValue(
+        tx: UserDefiTx | UserDefiClaimTx | UserAccountTx | UserPaymentTx
+    ): String {
+        // TODO: handle all transaction types here
+        if (tx instanceof UserPaymentTx)
+            return ethers.utils.formatEther(tx.value.value).toString()
+
+        return 'n/a'
+    }
 
     async function getTxs() {
         const rawTransactions = await user.getTxs()
@@ -25,6 +59,8 @@ function Transactions({ user }: TransactionsProps) {
             rawTransactions.map((tx) => {
                 return {
                     txId: tx.txId.toString(),
+                    txType: getTxType(tx),
+                    txValue: getTxValue(tx),
                 }
             })
         )
@@ -38,9 +74,32 @@ function Transactions({ user }: TransactionsProps) {
             clearInterval(txsInterval)
         }
     })
-    return <div>
-      { txs && txs.map(tx => <div  >{ tx.txId }</div>) }
-    </div>
+
+    if (!txs) return <></>
+
+    return (
+        <table className="table-auto text-right">
+            <thead>
+                <tr className="border-b border-black">
+                    <td className="px-4">id</td>
+                    <td className="px-4">type</td>
+                    <td className="px-4">value</td>
+                </tr>
+            </thead>
+            <tbody>
+                {txs.map((tx) => (
+                    <tr
+                        key={tx.txId.slice(-8)}
+                        className="border-b border-gray-300"
+                    >
+                        <td className="px-4">...{tx.txId.slice(-8)}</td>
+                        <td className="px-4">{tx.txType}</td>
+                        <td className="px-4">{tx.txValue}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
 }
 
 export default Transactions
